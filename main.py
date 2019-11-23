92a1d5fd3b003fe163a65d92a9c6a03fd6911742
MichalTurski <michal.antoni.turski@gmail.com>
2019-11-23 13:51:34 +0100

    Bug fixes.

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import pandas as pd
 import spans
 
 #%% read files
-islands_df = pd.read_csv('cpgIslandExt.txt', usecols=[1, 2, 3], header=['chr', 'start', 'stop'])
-lengths_df = pd.read_csv('hg19.chrom.sizes.txt', header=['chr', 'start', 'stop'])
-methyalation_df = pd.read_csv('HAIB.A549.EtOH.Rep.3.bed', usecols=[0, 1, 2], header=['chr', 'len'])
+islands_df = pd.read_csv('cpgIslandExt.txt', sep='\t', usecols=[1, 2, 3], header=None)
+islands_df.columns=['chr', 'start', 'stop']
+lengths_df = pd.read_csv('hg19.chrom.sizes.txt', sep='\t', index_col=0, header=None, squeeze=True)
+lengths = lengths_df.to_dict()
+methyalation_df = pd.read_csv('HAIB.A549.EtOH.Rep.3.bed', sep='\t', usecols=[0, 1, 2], header=None)
+methyalation_df.columns=['chr', 'start', 'stop']
 
 #%% filter inputs
 autosomal_chrom = set()
 for i in range(22):
-    autosomal_chrom.append('chr' + str(i + 1))
+    autosomal_chrom.add('chr' + str(i + 1))
 
 
 def filter_autosomal(df, autosomal_chrom):
@@ -17,7 +20,7 @@ def filter_autosomal(df, autosomal_chrom):
 
 
 islands_df = filter_autosomal(islands_df, autosomal_chrom)
-lengths_df = filter_autosomal(lengths_df, autosomal_chrom)
+# lengths_df = filter_autosomal(lengths_df, autosomal_chrom)
 methyalation_df = filter_autosomal(methyalation_df, autosomal_chrom)
 
 #%% find islands, shores, shelves and seas
@@ -34,9 +37,9 @@ def intervals_for_chrom(islands_chrom_df, chrom_len):
 
 
 for chrom in autosomal_chrom:
-    length = lengths_df.loc[chrom, 'len']
-    sea_interval = spans.intrangeset(spans.intrange(0, length))
-    shelf_interval = spans.intrangeset(None)
-    shore_interval = spans.intrangeset(None)
+    spans.intrange(0, lengths[chrom])
+    sea_interval = spans.intrangeset([spans.intrange(0, lengths[chrom])])
+    shelf_interval = spans.intrangeset([])
+    shore_interval = spans.intrangeset([])
 
 
