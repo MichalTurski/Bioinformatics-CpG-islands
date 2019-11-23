4107b6e7f2109530536529dc996f4d3b9a36a16f
MichalTurski <michal.antoni.turski@gmail.com>
2019-11-23 14:19:33 +0100

    Counting intervals for each sea/shelf/shore.

--- main.py
+++ main.py
@@ -27,19 +27,39 @@ methyalation_df = filter_autosomal(methyalation_df, autosomal_chrom)
 
 
 def intervals_for_chrom(islands_chrom_df, chrom_len):
+    # Border numbers section:
+    shore_limit = 2000
+    shelf_limit =  shore_limit + 2000
+
     chrom_interval = spans.intrange(0, chrom_len)
-    sea_interval = spans.intrangeset(chrom_interval)
-    shelf_interval = spans.intrangeset(None)
-    shore_interval = spans.intrangeset(None)
-    island_interval = spans.intrangeset(None)
+    sea_interval = spans.intrangeset([chrom_interval])
+    shelf_interval = spans.intrangeset([])
+    shore_interval = spans.intrangeset([])
+    island_interval = spans.intrangeset([])
     for _, island in islands_chrom_df.iterrows():
         island_interval.add(spans.intrange(island['start'], island['stop']).intersection(chrom_interval))
+        shore_interval.add(spans.intrange(island['start'] - shore_limit, island['stop'] + shore_limit)
+                           .intersection(chrom_interval))
+        shelf_interval.add(spans.intrange(island['start']-shelf_limit, island['stop']+shelf_limit)
+                           .intersection(chrom_interval))
+    print('done inner loop')
+    sea_interval = sea_interval.difference(shelf_interval)
+    print('done sea interval')
+    shelf_interval = shelf_interval.difference(shore_interval)
+    print('done shelf interval')
+    shore_interval = shore_interval.difference(island_interval)
+    print('done shore interval')
+
 
 
 for chrom in autosomal_chrom:
-    spans.intrange(0, lengths[chrom])
-    sea_interval = spans.intrangeset([spans.intrange(0, lengths[chrom])])
-    shelf_interval = spans.intrangeset([])
-    shore_interval = spans.intrangeset([])
+    islands_chrom_df  = islands_df[islands_df['chr'] == chrom]
+    intervals_for_chrom(islands_chrom_df, lengths[chrom])
+    # spans.intrange(0, lengths[chrom])
+    # sea_interval = spans.intrangeset([spans.intrange(0, lengths[chrom])])
+    # shelf_interval = spans.intrangeset([])
+    # shore_interval = spans.intrangeset([])
+
+
 
 
